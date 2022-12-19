57cf9e58051859e576ddb51686d5a52148883202
snowflaketesemail <121052897+snowflaketesemail@users.noreply.github.com>
2022-12-19 12:17:52 -0600

    Update streamlit_app.py
    added menu header
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Wealthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
